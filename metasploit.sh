2058e81494e676827e0f63d779e3502afd67a077
icammaci <59759267+icammaci@users.noreply.github.com>
2020-07-25 16:28:23 +0700

    Add files via upload

---
+++ metasploit.sh
@@ -0,0 +1,75 @@
+pkg install figlet -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install curl -y
+pkg install perl -y
+pip2 install --upgrade pip
+pkg install ruby -y
+gem install lolcat -y
+
+clear
+toilet -f smslant -F gay 'METASPLOIT INSTALLER BY MR CRACKED'
+sleep 1
+echo "\033[33;1m Metasploit Installer" "\033[37;1m From" "\033[36;1mMahiska Cyber Team"
+
+pkg install figlet -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install curl -y
+pkg install perl -y
+pip2 install --upgrade pip
+pkg install ruby -y
+gem install lolcat -y
+
+clear
+toilet -f smslant -F gay 'METASPLOIT INSTALLER BY MAHISKA CYBER TEAM'
+sleep 1
+echo "\033[33;1m Metasploit Installer" "\033[37;1m From" "\033[36;1mMahiska Cyber Team"
+sleep 1
+echo "\033[31;1m Subscribe channel YouTube" "\033[36;1mMR CRACKED"
+
+echo " "
+echo "____________________________________________________________"|lolcat
+echo "\033[30;1m                 Syarat" "\033[34;1m&" "\033[30;1mKetentuan            "
+sleep 1
+echo "\033[32;1mPastikan Anda Tidak Pernah Menginstal Metasploit Sebelumnya"
+sleep 1
+echo "\033[32;1mSisakan Ruang Penyimpanan Minimal 300 MB"
+sleep 1
+echo "\033[32;1mJaringan Anda Harus Dalam Keadaan Online dan stabil"
+sleep 1
+echo "\033[32;1mAnda Harus Sabar Karena Proses Sangat Lama"
+sleep 1
+echo "\033[31;1mDilarang MeRecode Tools Ini"
+sleep 1
+echo "\033[31;1mJangan Sampai Layar Ponsel Anda Mati"
+sleep 1
+echo "\033[31;1mJangan Keluar Dari Aplikasi Termux"
+sleep 1
+echo "\033[31;1mJangan Lupa Suscribe Channel YouTube Kami"
+echo "____________________________________________________________"|lolcat
+sleep 3
+echo "\033[35;1m            Bila Sakit Semoga Cepat Sembuh"
+echo "------------------------------------------------------------"|lolcat
+read -p "Siap? [Y/T]=" MR_CRACKED
+if [ $MR_CRACKED = Y ] || [ $MR_CRACKED = y ]
+then
+echo "\033[32;1mProses Penginstalan Package Yang Dibutuhkan"
+pkg install unstable-repo -y
+pkg install metasploit -y
+echo "\033[32;1m INSTALASI SELESAI"
+echo "\033[33;1m Author: " "\033[34;1mMR CRACKED"
+sleep 1
+echo "untuk menjalankan metasploit ketik msfconsole"
+echo "mulai menjalankan metasploit...."
+sleep 2
+msfconsole
+fi
+
+if [ $MR_CRACKED = T ] || [ $MR_CRACKED = t ]
+then
+echo "Mungkin Lain Kali"
+exit
+fi
